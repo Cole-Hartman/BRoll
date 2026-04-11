252bc18062de3ab6e3345d7b2d8dae1fb2999f2b
Cole Hartman <cole-hartman@github.com>
2026-04-11 00:33:19 -0700

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { Layout } from './components/layout/Layout';
 import { Home } from './pages/Home';
@@ -65,10 +65,10 @@ function AppRoutes() {
 
 export function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
