e5e7e6c0f387d196fe5bebbd2fc5c41751d7a312
Cole Hartman <cole-hartman@github.com>
2026-04-12 14:52:27 -0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    base: '/BRoll/',
+    base: '/',
     plugins: [react()],
     server: {
       port: 3000,
